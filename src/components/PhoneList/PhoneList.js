4e1c68e035679b4334eea656a121d7b12a056fef
AlexandraRomanova <65874420+AlexandraRomanova@users.noreply.github.com>
2022-01-29 01:05:20 +0200

    iconButton

--- src/components/PhoneList/PhoneList.js
+++ src/components/PhoneList/PhoneList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import s from "./PhoneList.module.css";
+import IconButton from "../IconButton/Iconbutton";
+import { ReactComponent as DeleteButton } from "../icons/close.svg";
 
 const PhoneList = ({ contacts, onDeleteContact }) => (
   <ul className={s.list}>
@@ -8,13 +10,19 @@ const PhoneList = ({ contacts, onDeleteContact }) => (
       <li className={s.item} key={id}>
         <p className={s.text}>{name}</p>
         <p className={s.text}>{number}</p>
-        <button
+        <IconButton
+          onClick={() => onDeleteContact(id)}
+          aria-label="Удалить контакт"
+        >
+          <DeleteButton width="30" height="30" />
+        </IconButton>
+        {/* <button
           className={s.deleteBtn}
           type="button"
           onClick={() => onDeleteContact(id)}
         >
           Delete
-        </button>
+        </button> */}
       </li>
     ))}
   </ul>
